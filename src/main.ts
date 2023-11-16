5c6319b9b5f25238c2ffc123b680486a14400da3
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-11-16 01:42:08 -0300

    subir

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle ('Mongo')
+  .setDescription ('API de Mongo')
+  .setVersion ('1.0')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup ('/api', app, document)
+
+
   await app.listen(3000);
 }
 bootstrap();
+
+
+
