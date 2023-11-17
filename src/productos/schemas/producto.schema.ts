d3ccdd44e3f48ecfbac5173358902cf26a37c8f9
Alejandra Ayala <onlyourfeeling@hotmail.com>
2023-11-17 18:30:30 -0300

    subir

--- src/productos/schemas/producto.schema.ts
+++ src/productos/schemas/producto.schema.ts
@@ -1,6 +1,28 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
 
 @Schema ({collection:"producto"})
 export class producto{
+
+    @Prop({name:"id_producto"})
+    ID_Producto:string;
+    
+    @Prop({name:"nombre"})
+    Nombre:string;
+    
+    @Prop({name:"descripcion"})
+    Descripcion:string;
+
+    @Prop({name:"cantidad"})
+    Cantidad:string;
+
+    @Prop({name:"precio_unitario"})
+    Precio_Unitario:string;
+
+    @Prop({name:"fecha_venta"})
+    Fecha_Venta:string;
+
+    @Prop ({name:"stock"})
+    Stock:string;
+    
 
 }
--- src/ventas/schemas/venta.schema.ts
+++ src/ventas/schemas/venta.schema.ts
@@ -1,6 +1,18 @@
-import { Schema } from "@nestjs/mongoose";
+import { Prop, Schema } from "@nestjs/mongoose";
 
 @Schema ({collection:"venta"})
 export class Venta {
+
+    @Prop({name:"producto"})
+    ID_Producto :string;
+
+    @Prop({name:"cantidad"})
+    Cantidad:string;
+    
+    @Prop({name:"precio_unitario"})
+    Precio_Unitario:string;
+    
+    @Prop({name:"fecha_venta"})
+    Fecha_Venta:string;
 
 }
